85a0b6bd61a5c9e2f0be78636171746e411a0d0e
Sadat Aerafin <18-38132-2@student.aiub.edu>
2021-08-18 19:22:49 +0600

    Add files via upload

---
+++ Lab 5/view/footer.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+	<hr>
+<p><h3>Payment Methods</h3></p> <br>
+<img src="img/pay2.jpg"width="500" height="150">
+<p align="center"> All rights reserved by &reg; e-commerce</p> <br>
+<p align="center">&copy; e-commerce 2021</p>
+</body>
+</html>
---
+++ Lab 5/view/home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Public Home</title>
+</head>
+<body>
+<?php include 'header.php'; ?>
+<h2 style="color:#53b8d4;">Welcome to 'X'E-Commerce</h2>
+<?php include 'footer.php'; ?>
+
+</body>
+</html>
